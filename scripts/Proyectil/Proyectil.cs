18bc55bdfac64f1991b3fa78f3727e5177c22157
David Rocer <drocher951@gmail.com>
2026-03-23 18:16:10 +0100

    Municion Rafaga Añadida

--- scripts/Proyectil/Proyectil.cs
+++ scripts/Proyectil/Proyectil.cs
@@ -1,33 +1,32 @@
 using Godot;
+using System.Collections.Generic;
 
 public partial class Proyectil : RigidBody2D
 {
-    [Export] public int Dmg;
+    [Export] public int Dmg = 1;
     [Export] public bool EsDeFuego = false;
+    [Export] public bool EsRafaga = false; // ESTE debe estar marcado en la escena de ráfaga
     [Export] public float DuracionFuego = 3.0f;
+
+    private List<Node2D> _enemigosGolpeados = new List<Node2D>();
 
     public override void _Ready()
     {
-        // Importante: En el Inspector de la bala, 'Contact Monitor' debe ser ON 
-        // y 'Max Contacts Reported' debe ser al menos 1.
+        // Importante para RigidBody2D
+        ContactMonitor = true;
+        MaxContactsReported = 5;
         BodyEntered += OnBodyEntered;
     }
 
     private void OnBodyEntered(Node body)
     {
-        if (body is Enemigo enemigo)
+        if (body is Enemigo enemigo && !_enemigosGolpeados.Contains(enemigo))
         {
-            if (EsDeFuego)
-                enemigo.Quemarse(Dmg, DuracionFuego);
-            else
-                enemigo.RecibirDmg(Dmg);
-            
-            GD.Print("¡Impacto!");
+            _enemigosGolpeados.Add(enemigo);
+            if (EsDeFuego) enemigo.Quemarse(Dmg, DuracionFuego);
+            else enemigo.RecibirDmg(Dmg);
         }
     }
 
-    private void OnScreenExited()
-    {
-        QueueFree();
-    }
+    private void OnScreenExited() => QueueFree();
 }
